3f2272f369a330f1b063c87dc93c36b19ca1d915
Abdussalam-Mujeeb-ur-rahman <mjbabdussalam@gmail.com>
2024-01-06 11:25:25 +0100

    removed scrollReveal, cube.css and remixicons

--- scripts/script.js
+++ scripts/script.js
@@ -14,28 +14,6 @@ harmburgers.forEach((harmburger) => {
     })
 })
 
-// scroll reveal
-ScrollReveal({
-    // reset: true, //makes the animation repeat over and over again.
-    distance: "80px",
-    duration: 2000,
-    delay: 200,
-  });
-
-
-  ScrollReveal().reveal(".welcome_image, .doings_talks_d, .socials", { origin: "bottom" });
-  ScrollReveal().reveal(".message", { origin: "left", delay: "1000" });
-  ScrollReveal().reveal(".sure, .work_action .click_here", { origin: "right", delay: "2000" });
-  ScrollReveal().reveal(".space, .about_details .c, .here", { origin: "left", delay: "3000" });
-
-  ScrollReveal().reveal(".about_details .a, .section_subtitle, .work_experience h2, .experience_title, .work_action .learn, .doings_talks_a, .doings_talks_c, .doing:nth-of-type(odd)", { origin: "left" });
-  ScrollReveal().reveal(".about_details .b, .work_action .started", { origin: "right", delay: "1000" });
-  ScrollReveal().reveal(".about_details .d", { origin: "left", delay: "4000" });
-  ScrollReveal().reveal(".about_details .f", { origin: "right", delay: "4000" });
-  ScrollReveal().reveal(".about_details .e", { origin: "right", delay: "5000" });
-
-  ScrollReveal().reveal(".experience_date, .experience_description, .doings_talks_b, .doing:nth-of-type(even)", { origin: "right" });
-
 
 // =============================== SHOW SCROLL UP =================================
 const scrollUpAndHideNav = () => {
